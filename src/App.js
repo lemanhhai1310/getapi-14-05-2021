df8fcd8e588c8da9f30f8265deb1fcd726742430
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-05-14 20:04:32 +0700

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
-import './App.css';
 
 function App() {
     const dummyMovies = [
@@ -19,14 +18,38 @@ function App() {
         },
     ];
 
+    const [movies,setMovies] = useState([]);
+
+    async function fetchMoviesHandler(){
+        await fetch('https://swapi.dev/api/films/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const tranformedMovies = data.results.map(movieData => {
+                    return {
+                        id:movieData.episode_id,
+                        title:movieData.title,
+                        openingText:movieData.opening_crawl,
+                        releaseDate:movieData.release_date,
+                    }
+                });
+                setMovies(tranformedMovies);
+            });
+    }
+
     return (
         <React.Fragment>
-            <section>
-                <button>Fetch Movies</button>
-            </section>
-            <section>
-                <MoviesList movies={dummyMovies}/>
-            </section>
+            <div className="uk-section uk-background-muted">
+                <div className="uk-container uk-container-xsmall">
+                    <section>
+                        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+                    </section>
+                    <section>
+                        <MoviesList movies={movies}/>
+                    </section>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
